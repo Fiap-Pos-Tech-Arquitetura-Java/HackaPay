ae714addd4606692685152dce7642a7e88a6e0ae
andersonwagner <anderson.wagner@gmail.com>
2024-08-04 16:32:09 +0200

    correcao das rotas.

--- src/main/java/br/com/fiap/postech/hackapay/HackaPayApplication.java
+++ src/main/java/br/com/fiap/postech/hackapay/HackaPayApplication.java
@@ -14,35 +14,35 @@ public class HackaPayApplication {
         SpringApplication.run(HackaPayApplication.class, args);
     }
 
-    @Value("${hackapay.user.host}")
-    String userHost;
+    @Value("${hackapay.user}")
+    String user;
 
-    @Value("${hackapay.cliente.host}")
-    String clienteHost;
+    @Value("${hackapay.cliente}")
+    String cliente;
 
-    @Value("${hackapay.cartao.host}")
-    String cartaoHost;
+    @Value("${hackapay.cartao}")
+    String cartao;
 
-    @Value("${ackapay.pagamentos.hosth}")
-    String pagamentosHost;
+    @Value("${hackapay.pagamentos}")
+    String pagamentos;
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("usuario", r -> r.path("/api/user/**")
                         .and()
-                        .not( p->p.path("/hackapay/user/findByLogin/**"))
+                        .not( p->p.path("/api/user/findByLogin/**"))
                         .filters(f -> f.stripPrefix(1))
-                        .uri("http://" + userHost + ":8080"))
-                .route("cliente", r -> r.path("api/cliente/**")
+                        .uri("http://" + user + ":8080" + user))
+                .route("cliente", r -> r.path("/api/cliente/**")
                         .filters(f -> f.stripPrefix(1))
-                        .uri("http://" + clienteHost + ":8081"))
+                        .uri("http://" + cliente + ":8081" + cliente))
                 .route("cartao", r -> r.path("/api/cartao/**")
                         .filters(f -> f.stripPrefix(1))
-                        .uri("http://" + cartaoHost + ":8082"))
+                        .uri("http://" + cartao + ":8082" + cartao))
                 .route("pagamento", r -> r.path("/api/pagamentos/**")
                         .filters(f -> f.stripPrefix(1))
-                        .uri("http://" + pagamentosHost + ":8083"))
+                        .uri("http://" + pagamentos + ":8083" + pagamentos))
                 .build();
     }
 }
